880a328e40d4f34fb3fe9458f793d3b2a5d0849c
Fabio Freitas <fabiofreitasdev@gmail.com>
2023-01-22 20:48:35 -0300

    feat: Post component and its style

---
+++ src/components/Post.jsx
@@ -0,0 +1,66 @@
+import { Avatar } from './Avatar';
+import { Commment } from './Comment';
+import styles from './Post.module.css';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+
+export function Post({ author, publishedAt, content }) {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+      <div className={styles.content}>
+
+        {
+          content.map(line => {
+            if (line.type === 'paragraph') {
+              return <p key={line.content}>{line.content}</p>;
+            } else if (line.type === 'link') {
+              return <p key={line.content}><a href="">{line.content}</a></p>;
+            }
+          })
+        }
+      </div>
+
+      <form
+        className={styles.commentForm}
+      >
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário...'
+          name="comment"
+          required
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => <Commment
+          key={comment}
+          content={comment}
+          onDeleteComment={deleteComment}
+        />)}
+      </div>
+
+    </article>
+  )
+}
