cd5ebf8c36268a2743caca01dde882212b21ec92
Fabio Freitas <fabiofreitasdev@gmail.com>
2023-01-22 22:08:16 -0300

    feat: App component and its style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import styles from './App.module.css';
+import { SideBar } from './components/SideBar';
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/pedro-demeu.png',
+      name: "Pedro Miguel",
+      role: "Desenvolvedor Frontend"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-13 20:42:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: "Maik Brito",
+      role: "Instrutor na Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-13 20:44:00'),
+  },
+]
+
+// iteração
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
+    <>
+      <Header></Header>
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          {posts.map(post => <Post
+            key={post.id}
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt} />)}
+        </main>
+      </div>
+    </>
   )
 }
 
